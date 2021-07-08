20a1fa33609c65893ee1ce2b473f3e596804fb5f
Melinda <melinda.sava.td@gmail.com>
2021-07-08 15:26:14 +0200

    typescript computed plus simple filter with .sort()

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div><JobList :jobs="jobs" /></div>
+<div class="app">
+  <header>
+    <button @click="handleClick('title')">order by title</button>
+    <button @click="handleClick('location')">order by location</button>
+    <button @click="handleClick('salary')">order by salary</button>
+  </header>
+  <JobList :jobs="jobs" :order="order" />
+</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import Job from "./types/Job"
+import OrderTerm from "./types/OrderTerm"
 import JobList from "./components/JobList.vue"
 
 export default defineComponent({
@@ -18,11 +26,33 @@ export default defineComponent({
       { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
     ])
+    const order = ref<OrderTerm>('title')
 
-    return { jobs }
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+      
+    }
+
+    return { jobs, handleClick, order }
   },
 });
 </script>
 
 <style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 </style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="job-list">
+    <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
@@ -15,18 +16,30 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import Job from '@/types/Job'
+import OrderTerm from '@/types/OrderTerm'
 
 export default defineComponent({
   props: {
     jobs: {
       required: true,
       type: Array as PropType<Job[]>
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>
     }
   },
-  // setup() {  
-  // },
+  setup(props) {  
+    const orderedJobs = computed((term) => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1
+      })
+    })
+
+    return { orderedJobs }
+  },
 })
 </script>
 
